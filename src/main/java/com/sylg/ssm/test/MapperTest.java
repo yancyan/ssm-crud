c01cb818d2ecfc6b3bc57540de03a7a198662f47
yancyan <614049594@qq.com>
2017-07-14 21:33:51 +0800

    update dao单表查询测试成功！

---
+++ src/main/java/com/sylg/ssm/test/MapperTest.java
@@ -0,0 +1,47 @@
+package com.sylg.ssm.test;
+
+import com.sylg.ssm.bean.Employee;
+import com.sylg.ssm.dao.DeptMapper;
+import com.sylg.ssm.dao.EmployeeMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 2017/7/13.
+ *
+ * 导入spring单元测试的模块, 可以自动注入我们需要的组件
+ *  1， 导入springtest单元测试模块
+ *  2， @ContextConfiguration指定spring配置文件的位置
+ *  3，直接Autowired
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class MapperTest {
+    @Autowired
+    DeptMapper deptMapper;
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    @Test
+    public void test() throws Exception {
+        /*创建IOC 容器*/
+        /*ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        DeptMapper deptMapper = context.getBean(DeptMapper.class);*/
+        System.out.println(deptMapper);
+
+        //Dept dept = deptMapper.selectByPrimaryKey(1);
+        //Employee emp = employeeMapper.selectByPrimaryKey(1);
+        //System.out.println(dept.toString());
+        //System.out.println(emp.toString());
+
+        employeeMapper.insert(new Employee(null, "张da明", "男", "zhangxiaoming@qq.com", 1));
+
+        //Employee em = employeeMapper.selectByPrimaryKeyWithDept(1);
+       // System.out.println(em.toString());
+    }
+}
